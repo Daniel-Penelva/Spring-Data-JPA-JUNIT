9f2845fc4d461a66cb88ad54c9a5fceb5fd81d57
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-06 04:04:48 -0300

    Criando Projeto Maven

---
+++ src/test/java/br/com/daniel/produtosapptest/ProdutosapptestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.produtosapptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapptestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
