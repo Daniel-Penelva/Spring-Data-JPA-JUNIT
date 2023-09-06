eb0e798ee515738003bad6c7f83ffff2a8f1d000
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-06 05:03:55 -0300

    Criando o ProdutoRepository (extendendo CrudRepository)

---
+++ src/main/java/br/com/daniel/produtosapptest/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package br.com.daniel.produtosapptest.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.daniel.produtosapptest.model.Produto;
+
+public interface ProdutoRepository extends CrudRepository<Produto, Integer>{
+    
+}
