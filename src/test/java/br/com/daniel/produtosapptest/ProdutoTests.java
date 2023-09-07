5b6f3b7b9962449ca9cc482c05e3425e0a835ef6
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-06 23:24:18 -0300

    Utilizando a anotação @TestMethodOrder

--- src/test/java/br/com/daniel/produtosapptest/ProdutoTests.java
+++ src/test/java/br/com/daniel/produtosapptest/ProdutoTests.java
@@ -6,9 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.List;
-
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
@@ -20,7 +20,8 @@ import br.com.daniel.produtosapptest.repository.ProdutoRepository;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
-public class ProdutoTests {
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
+public class ProdutoTests{
 
     /*OBS. Vamos usar o H2 Database para testes unitários para isso vamos adicionar a dependência H2Database no pom.xml, vamos crair um ambiente 
     de teste separado que não afeta o banco de dados de produção. Isso é fundamental para garantir que os testes sejam repetíveis, previsíveis 
@@ -29,7 +30,7 @@ public class ProdutoTests {
     @Autowired
     ProdutoRepository produtoRepository;
 
-    //@Test
+    @Test
     @Rollback(false)
     public void testCriarProduto(){
         Produto produto = new Produto("teste", 4000);
@@ -39,9 +40,9 @@ public class ProdutoTests {
         assertNotNull(criarProduto);
     }
 
-    //@Test
+    @Test
     public void testBuscarProdutoPorNome(){
-        String nome = "TV Samsung HD";
+        String nome = "X-Box One";
 
         Produto produto = produtoRepository.findByNome(nome);
 
@@ -50,7 +51,7 @@ public class ProdutoTests {
         System.out.println("É o mesmo valor buscado"); 
     }
 
-    //@Test
+    @Test
     public void testBuscarProdutoPorNomeNoExists(){
         String nome = "IPhone 11";
 
@@ -60,14 +61,14 @@ public class ProdutoTests {
         System.out.println("Não é o mesmo valor buscado"); 
     }
 
-    //@Test
+    @Test
     @Rollback(false)
     public void testAtualizarProduto(){
         // Cria um novo nome de produto
-        String nomeProduto = "TV Samsung HD 72 polegadas";
+        String nomeProduto = "TV Samsung HD 72 polegadas + caixa de som";
 
         // Atualiza esse novo produto passando o id para ser encontrado
-        Produto produto = new Produto(nomeProduto, 6000);
+        Produto produto = new Produto(nomeProduto, 8000);
         produto.setId(1);
 
         // Salva o produto 
@@ -81,7 +82,7 @@ public class ProdutoTests {
          assertEquals(atualizarProduto.getNome(), nomeProduto);
     }
 
-    //@Test
+    @Test
     public void testListarProduto(){
         Iterable<Produto> produtos = produtoRepository.findAll();
 
@@ -103,7 +104,7 @@ public class ProdutoTests {
 
     @Test
     public void testDeletarProduto(){
-        Integer id = 1;
+        Integer id = 3;
 
         // Verifica se um objeto de produto com o identificador especificado existe no banco de dados.
         boolean existeProdutoId = produtoRepository.findById(id).isPresent(); // true
